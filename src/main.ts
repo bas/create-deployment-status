7c5aad19de781917604fa60ba96ab264f17ec098
Bas Peters <bas@github.com>
2019-10-01 19:20:52 +0200

    Add type for state

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
+type DeploymentStatusState =
+  | "error"
+  | "failure"
+  | "inactive"
+  | "in_progress"
+  | "queued"
+  | "pending"
+  | "success";
+
+function asDeploymentStatusState(value: string): DeploymentStatusState {
+  switch (value) {
+    case "error":
+    case "failure":
+    case "inactive":
+    case "in_progress":
+    case "queued":
+    case "pending":
+    case "success":
+      return value;
+  }
+
+  throw new Error('Unknown deployment status')
+}
+
 async function run() {
   try {
     const token = core.getInput('repo_token', { required: true });
     const deploymentId: number = Number(core.getInput('deployment_id', { required: true }));
-    const state = core.getInput('state', { required: true });
+    const state: DeploymentStatusState = asDeploymentStatusState(core.getInput("state", { required: true }));
     const targetUrl = core.getInput('target_url');
     const logUrl = core.getInput('log_url');
 
@@ -21,7 +45,7 @@ async function run() {
 async function createDeploymentStatus(
   client: github.GitHub,
   deploymentId: number,
-  state: any,
+  state: DeploymentStatusState,
   targetUrl: string,
   logUrl: string
 ) {
